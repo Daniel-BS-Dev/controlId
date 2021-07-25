3cc330458dc64cc745a91f921a18fe986e951df6
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-07-25 12:32:36 -0300

    First commit

---
+++ src/test/java/com/dio/id_control/IdControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.id_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
